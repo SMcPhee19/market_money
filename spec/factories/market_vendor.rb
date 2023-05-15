45d608b9fc7dfee1bbade743a5527aeeea0da677
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-15 17:29:23 -0600

    feat: add factory for join table, refactor test

---
+++ spec/factories/market_vendor.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :market_vendor do
+    
+  end
+end
--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -1,7 +1,7 @@
 # spec/requests/api/v0/market_request_spec.rb
 require 'rails_helper'
 
-describe 'Market API' do
+describe '/api/v0/markets' do
   it 'sends a list of all markets' do
     create_list(:market, 10)
 
@@ -42,16 +42,22 @@ describe 'Market API' do
   it 'can return a new attribute, vendor_count' do
     market1 = create(:market)
     market2 = create(:market)
-    vendors = create_list(:vendor, 5, market: market1)
-    vendors = create_list(:vendor, 3, market: market2)
+    vendors = create_list(:vendor, 5)
+
+    create(:market_vendor, market_id: market1.id, vendor_id: vendors[0].id)
+    create(:market_vendor, market_id: market1.id, vendor_id: vendors[1].id)
+    create(:market_vendor, market_id: market1.id, vendor_id: vendors[2].id)
+
+    create(:market_vendor, market_id: market2.id, vendor_id: vendors[3].id)
+    create(:market_vendor, market_id: market2.id, vendor_id: vendors[4].id)
 
     get '/api/v0/markets'
 
     markets = JSON.parse(response.body, symbolize_names: true)
-require 'pry'; binding.pry
+
     expect(markets[:data][0][:attributes]).to have_key(:vendor_count)
-    expect(markets[:data][0][:attributes][:vendor_count]).to eq(5)
+    expect(markets[:data][0][:attributes][:vendor_count]).to eq(3)
     expect(markets[:data][1][:attributes]).to have_key(:vendor_count)
-    expect(markets[:data][1][:attributes][:vendor_count]).to eq(3)
+    expect(markets[:data][1][:attributes][:vendor_count]).to eq(2)
   end
 end
