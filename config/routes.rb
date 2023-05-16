9580c2226fa57e9fe280b9eca8357c60056405af
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-16 11:32:52 -0600

    feat: add un-nested show route for vendors to fit the pattern

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v0 do
+      resources :vendors, only: %i[show]
       resources :markets, only: %i[index show] do
         resources :vendors, only: %i[index]
       end
