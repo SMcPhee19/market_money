513a3ed677f9a5377327c489694aac5fc7f20569
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-16 16:53:02 -0600

    feat: custom validation for credit_accepted, created validations for all other attributes

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Vendor, type: :model do
     it { should have_many(:market_vendors) }
     it { should have_many(:markets).through(:market_vendors) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:contact_name) }
+    it { should validate_presence_of(:contact_phone) }
+  end
+
+  it 'credit_accepted_presence' do
+    vendor = Vendor.new
+
+    vendor.validate
+    expect(vendor.errors[:credit_accepted]).to include('must be provided')
+
+    vendor.credit_accepted = true
+    vendor.validate
+    expect(vendor.errors[:credit_accepted]).to be_empty
+
+    vendor.credit_accepted = false
+    vendor.validate
+    expect(vendor.errors[:credit_accepted]).to be_empty
+  end
 end
