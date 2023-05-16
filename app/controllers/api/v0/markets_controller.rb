9d5d2aa74f0ba94b99d02966b85a00223815bdf2
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-15 18:37:53 -0600

    feat: add route and controller method for single market

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -4,4 +4,8 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render(json: MarketSerializer.new(Market.all))
   end
+
+  def show
+    render(json: MarketSerializer.new(Market.find(params[:id])))
+  end
 end
