189335bddbfcea30c1caabe0e25b2d587280191c
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-17 17:42:37 -0600

    refactor: rubocop changes

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -4,14 +4,15 @@ module Api
       def create
         vendor = Vendor.find_by_id(params[:market_vendor][:vendor_id])
         market = Market.find_by_id(params[:market_vendor][:market_id])
-        market_vendor = MarketVendor.new(market: market, vendor: vendor)
-      
+        market_vendor = MarketVendor.new(market:, vendor:)
+
         if market_vendor.save
           render json: MarketVendorSerializer.new(market_vendor), status: 201
         elsif market.nil? || vendor.nil?
           render json: { "errors": [{ "detail": "Validation failed: vendor or market doesn't exist" }] }, status: 404
         else
-          render json: { "errors": [{ "detail": "Validation failed: Market vendor asociation between market with market_id=#{params[:market_vendor][:market_id]} and vendor_id=#{params[:market_vendor][:vendor_id]} already exists" }] }, status: 422
+          render json: { "errors": [{ "detail": "Validation failed: Market vendor asociation between market with market_id=#{params[:market_vendor][:market_id]} and vendor_id=#{params[:market_vendor][:vendor_id]} already exists" }] },
+                 status: 422
         end
       end
 
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -47,11 +47,11 @@ describe 'creating a market vendor' do
   it 'sad path: market does not exist' do
     vendor1 = create(:vendor)
 
-    mv_params = ({"market_id": nil, "vendor_id": vendor1.id })
+    mv_params = { "market_id": nil, "vendor_id": vendor1.id }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: mv_params)
+    post '/api/v0/market_vendors', headers:, params: JSON.generate(market_vendor: mv_params)
 
     expect(response).to_not be_successful
     expect(response.status).to eq(404)
@@ -65,11 +65,11 @@ describe 'creating a market vendor' do
   it 'sad path: vendor does not exist' do
     market1 = create(:market)
 
-    mv_params = ({"market_id": market1.id, "vendor_id": nil })
+    mv_params = { "market_id": market1.id, "vendor_id": nil }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: mv_params)
+    post '/api/v0/market_vendors', headers:, params: JSON.generate(market_vendor: mv_params)
 
     expect(response).to_not be_successful
     expect(response.status).to eq(404)
@@ -86,15 +86,15 @@ describe 'creating a market vendor' do
 
     create(:market_vendor, market_id: market1.id, vendor_id: vendor1.id)
 
-    mv_params = ({"market_id": market1.id, "vendor_id": vendor1.id })
+    mv_params = { "market_id": market1.id, "vendor_id": vendor1.id }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: mv_params)
+    post '/api/v0/market_vendors', headers:, params: JSON.generate(market_vendor: mv_params)
 
     expect(response).to_not be_successful
     expect(response.status).to eq(422)
-    
+
     invalid = JSON.parse(response.body, symbolize_names: true)
 
     expect(invalid).to have_key(:errors)
