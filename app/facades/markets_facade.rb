5708527301399048d53d4a2ca3bfb2bb4d921184
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-18 13:27:38 -0600

    refactor: rubocop fixes

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -1,5 +1,5 @@
 class MarketsFacade
   def self.market_search(name, city, state)
-    Market.where("name ILIKE ? AND city ILIKE ? AND state ILIKE ?", "%#{name}%", "%#{city}%", "%#{state}%")
+    Market.where('name ILIKE ? AND city ILIKE ? AND state ILIKE ?', "%#{name}%", "%#{city}%", "%#{state}%")
   end
 end
