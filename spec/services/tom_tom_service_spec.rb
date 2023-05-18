f4950a84d96c7bc84a18b3044a84dde8583ad9e6
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-05-18 17:53:45 -0600

    refactor: get_atms is now an instance method

--- spec/services/tom_tom_service_spec.rb
+++ spec/services/tom_tom_service_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TomTomService do
   it 'can create a connnection, and get_atms', :vcr do
-    atms = TomTomService.get_atms(39.750783, -104.996435)
+    atms = TomTomService.new.get_atms(39.750783, -104.996435)
+    
     expect(atms).to be_a(Hash)
   end
 end
